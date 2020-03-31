5dfc9f3a108a43f8cf8ccf8569fef33887229471
pulasthieka <pulasthieka@yahoo.com>
2020-03-31 20:52:01 +0530

    Auto Scroll

--- mafia-app/src/app/chat/chat.component.ts
+++ mafia-app/src/app/chat/chat.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, OnDestroy, ViewChild } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  AfterViewChecked,
+  ElementRef,
+  ViewChild,
+  OnDestroy
+} from "@angular/core";
 import { WebsocketService } from "../websocket.service";
 import { FormControl } from "@angular/forms";
 import { Router } from "@angular/router";
@@ -9,18 +16,22 @@ import { ApiService } from "../api.service";
   templateUrl: "./chat.component.html",
   styleUrls: ["./chat.component.scss"]
 })
-export class ChatComponent implements OnInit {
-  // @ViewChild('chatbox') formValues;
+export class ChatComponent implements OnInit, AfterViewChecked, OnDestroy {
+  @ViewChild("scrollMe") private myScrollContainer: ElementRef;
   msg = new FormControl("");
   messages = [];
   disabled = false;
   player: string;
+  disableScrollDown = false;
+
   constructor(
     private socket: WebsocketService,
     private router: Router,
     private api: ApiService
   ) {}
-
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
   ngOnInit() {
     if (window.sessionStorage) {
       //c heck for webstorage compatibility
@@ -39,6 +50,7 @@ export class ChatComponent implements OnInit {
     this.socket.getChatMessages().subscribe((message: string) => {
       this.messages.push(JSON.parse(message));
     });
+    this.scrollToBottom();
   }
 
   ngOnDestroy() {
@@ -47,12 +59,34 @@ export class ChatComponent implements OnInit {
 
   sendChatMessage(text) {
     if (text) {
+      this.disableScrollDown = false;
       let message = JSON.stringify({
         username: this.player,
         message: text
       });
       this.socket.sendChatMessage(message);
       this.msg.setValue("");
+    }
+  }
+  scrollToBottom(): void {
+    if (this.disableScrollDown) {
+      return;
+    } else {
+      try {
+        this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+      } catch (err) {}
+    }
+  }
+
+  onScroll(): void {
+    let element = this.myScrollContainer.nativeElement;
+    let atBottom =
+      element.scrollHeight - element.scrollTop <= element.clientHeight + 50;
+    console.log(atBottom);
+    if (atBottom) {
+      this.disableScrollDown = false;
+    } else {
+      this.disableScrollDown = true;
     }
   }
 }
